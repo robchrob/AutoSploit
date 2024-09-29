e8b9acc95beb904d448729aa2e0ecf9dbc4cfa0b
Robert Chrobocinski <robert.chrobocinski@gmail.com>
2024-09-29 22:29:50 +0000

    dockerfule up

--- api_calls/shodan.py
+++ api_calls/shodan.py
@@ -1,5 +1,4 @@
 import json
-
 import requests
 
 from lib.settings import start_animation
@@ -9,7 +8,6 @@ from lib.settings import (
     HOST_FILE,
     write_to_file
 )
-
 
 class ShodanAPIHook(object):
 
@@ -29,19 +27,20 @@ class ShodanAPIHook(object):
         """
         connect to the API and grab all IP addresses associated with the provided query
         """
-        start_animation("searching Shodan with given query '{}'".format(self.query))
+        start_animation(
+            "searching Shodan with given query '{}'".format(self.query))
         discovered_shodan_hosts = set()
         try:
             req = requests.get(
-                API_URLS["shodan"].format(query=self.query, token=self.token),
+                "https://api.shodan.io/shodan/host/search",
+                params={"query": self.query, "key": self.token},
                 proxies=self.proxy, headers=self.user_agent
             )
             json_data = json.loads(req.content)
             for match in json_data["matches"]:
                 discovered_shodan_hosts.add(match["ip_str"])
-            write_to_file(discovered_shodan_hosts, self.host_file, mode=self.save_mode)
+            write_to_file(discovered_shodan_hosts,
+                          self.host_file, mode=self.save_mode)
             return True
         except Exception as e:
             raise AutoSploitAPIConnectionError(str(e))
-
-
